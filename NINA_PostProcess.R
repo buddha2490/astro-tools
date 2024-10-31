ce888ea3192820fe3c371ee4a16803f3ef5d8711
Brian Carter <bcarter6@me.com>
2024-10-31 16:28:45 -0400

    run on mbp13

--- NINA_PostProcess.R
+++ NINA_PostProcess.R
@@ -1,36 +1,51 @@
 library(openxlsx)
 library(magrittr)
 library(dplyr)
-library(DBI)
-library(RPostgreSQL)
 
 
-# Connect to Astro-PC2 before next script
-backups <- "/Volumes/astronomy/In Progress/ES127"
-src <- "/Volumes/Astro-SSD/In Progress/ES127"
-lapply(list.dirs(src, full.names = FALSE, recursive = FALSE), function(x) {
-  
-  subdirs <- list.dirs(file.path(backups, x), full.names = FALSE, recursive = FALSE)
-  srcdirs <- list.dirs(file.path(src, x), full.names = FALSE, recursive = FALSE)
-  new <- setdiff(srcdirs, subdirs)
-  
-  
-  file.copy(file.path(src, x, new),
-            file.path(backups, x),
-            recursive = TRUE)
-  
-  
+
+
+# Connect to astro-pc/users to get the most recent copy of the images
+#
+
+username <- Sys.getenv("AstroPCUsername")
+password <- Sys.getenv("AstroPCPassword")
+mbp14 <- Sys.getenv("mbp14")
+users <- paste0("open 'smb://", username, ":", password, "@astro-pc/users'")
+ssd <- paste0("open 'smb://", username, ":", password, "@astro-pc/Telescope'")
+laptop <- paste0("open 'smb://", username, ":", password, "@", mbp14, "/briancarter/Astronomy'")
+system(laptop)
+system(ssd)
+system(users)
+rm(users, ssd, username, password, mbp14, laptop, mbp)
+
+
+officeSSD <- "/Volumes/Astro-SSD/In Progress/ES127" # this is my SSD attached to office laptop
+pcSSD <- "/Volumes/Users/bcart/astronomy/In Progress/ES127" # this is the SSD internal to astro-pc
+laptop <- "/Volumes/Astronomy" # macbook pro 14", main computer
+
+
+# Write a function to copy everything from the internal Scope SSD to the office SSD
+# Ideally this is only a limited number of subs, because I transfer this every morning.
+# there is also a larger backup on the Samsung SSD attached to the telescope
+# I'm just leaving that for now to occasionally back up to the HDD in the office.
+
+myDirs <- list.dirs(pcSSD, recursive = FALSE, full.names = TRUE)
+lapply(myDirs, function(x) {
+  file.copy(x, officeSSD, recursive = TRUE, overwrite = FALSE)
 })
 
 
 myLog <- function(path, object) {
   src <- glue::glue("{path}/{object}")
-  subdirs <- list.dirs(src, full.names = TRUE, recursive = FALSE)
+  subdirs <- list.dirs(src, full.names = TRUE, recursive = TRUE)
   
   metadata <- readr::read_csv(file.path(src, "object_info.csv"))
   
   
   foo <- lapply(subdirs, function(x) {
+    
+    
     
     subfolder <- basename(x) |>
       stringr::str_remove(glue::glue("{object}_"))
@@ -58,35 +73,39 @@ myLog <- function(path, object) {
     files <- list.files(x, pattern = ".fit") |> 
       length()
     
-    df <- data.frame(
-      Object = object, 
-      Name = metadata$Name |> as.character(),
-      Constellation = metadata$Constellation |> as.character(),
-      Type = metadata$Type |> as.character(),
-      Date = date,
-      Filter = filter,
-      Subfolder = subfolder,
-      Exposure = exposure,
-      TotalSubs = files,
-      TotalTimeHrs = (files * exposure / 3600) |> round(2)
-    )
-    return(df)
-  })
+    if (files != 0) {
+      
+      df <- data.frame(
+        Object = object, 
+        Name = metadata$Name |> as.character(),
+        Constellation = metadata$Constellation |> as.character(),
+        Type = metadata$Type |> as.character(),
+        Date = date,
+        Filter = filter,
+        Subfolder = subfolder,
+        Exposure = exposure,
+        TotalSubs = files,
+        TotalTimeHrs = (files * exposure / 3600) |> round(2)
+      )
+      return(df)
+    }
+  }
+  )
   do.call("rbind", foo)
 }
 myLogWrapper <- function(path) {
   
-  logPath <- "/users/briancarter/Astronomy/Imaging Logs"
-  backupLog <- file.path(logPath, "backups")
-  dir.create(backupLog, showWarnings = FALSE)
-  
-  logs <- list.files("/users/briancarter/Astronomy/Imaging Logs", pattern = ".xlsx", full.names = TRUE) 
-  lapply(logs, function(x) {
-    file.copy(x, file.path(backupLog))
-    file.remove(x)
-  })
-  
+  # Remove old version of log file
+  logs <- list.files(laptop, pattern = "Imaging Log", full.names = TRUE) 
+  file.remove(logs)
+
   dirs <- list.dirs(path, full.names = FALSE, recursive = FALSE)
+  images <- data.frame()
+  for (i in 1:length(dirs)) {
+    images <- rbind(images, myLog(path, dirs[i]))
+  }
+  
+  
   images <- lapply(dirs, function(x) {
     myLog(path, x)
   }) %>%
@@ -98,27 +117,13 @@ myLogWrapper <- function(path) {
                      TotalTimeHrs = sum(TotalTimeHrs))
   
   
-  wb <- createWorkbook(glue::glue("/users/briancarter/Astronomy/Imaging Logs/Imaging Log - {Sys.Date()}.xlsx"))
+  wb <- createWorkbook(glue::glue("{laptop}/Imaging Log - {Sys.Date()}.xlsx"))
   addWorksheet(wb, "Images")
   addWorksheet(wb, "Summary")
   writeData(wb, "Images", images)
   writeData(wb, "Summary", summaries)
-  saveWorkbook(wb, glue::glue("/users/briancarter/Astronomy/Imaging Logs/Imaging Log - {Sys.Date()}.xlsx"))
+  saveWorkbook(wb, glue::glue("{laptop}/Imaging Log - {Sys.Date()}.xlsx"))
   
-  username <- Sys.getenv("username")
-  password <- Sys.getenv("password")
-  
-  myDB <- dbConnect(dbDriver("PostgreSQL"),
-                    dbname = "astro",
-                    host = "astro-pc2",
-                    port = 5432,
-                    user = username,
-                    password = password)
-  
-  dbWriteTable(myDB, "ImagingLog", images, overwrite = TRUE)
-  dbWriteTable(myDB, "ImagingLogSummary", summaries, overwrite = TRUE)
-  
-  dbDisconnect(myDB)
   
 }
 myLogWrapper("/Volumes/Astro-SSD/In Progress/ES127")
