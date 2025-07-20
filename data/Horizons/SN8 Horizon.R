8c1d26cc6dba7dbf250e9290f795a23b5e74a540
Brian Carter <bcarter6@me.com>
2025-07-20 16:24:05 -0400

    horizon.png

--- data/Horizons/SN8 Horizon.R
+++ data/Horizons/SN8 Horizon.R
@@ -4,20 +4,38 @@ library(dplyr)
 list.files("data/horizons")
 
 
-df <- readr::read_csv("data/horizons/horizon_19Oct2024 v2.hrz", col_names = F)
+makegroup <- function(group_size = 5) {
+  
+  group <- data.frame()
+  for (i in 1:69) {
+    group <- data.frame(Group = rep(i, group_size)) %>% bind_rows(group)
+  } 
+  return(group)
+} 
+
+df <- readr::read_table("data/horizons/backyard.hrz", col_names = F)
 colnames(df) <- c("AZ", "ALT")
+
+df2 <- data.frame(AZ = as.numeric(1:360))
+
+df <- full_join(df, df2, "AZ") %>%
+  arrange(desc(AZ)) %>%
+  mutate(ALT = ifelse(is.na(ALT), lag(ALT), ALT))
+
 
 df$NewAz <- 360- df$AZ
 df$NewAlt <- round(df$ALT / 26 * 100)
-df$NODE <- glue::glue("Node : {df$NewAz}, {df$NewAlt}")
 df <- df %>%
+  bind_rows(data.frame(NewAz = 354, NewAlt = 212, AZ =6, ALT = 55)) %>%
   arrange(desc(NewAz)) %>%
   group_by(NewAz) %>%
   filter(row_number() == 1) %>%
   ungroup() %>%
+  arrange(desc(AZ))
+
+df <- df %>%
+  mutate(NODE = glue::glue("Node : {NewAz}, {NewAlt}")) %>%
   select(NODE)
 
-
-head(df)
 
 write.table(df, "data/horizons/SN8_horizon.txt", row.names = F, col.names = F, quote = F)
