1f28872d32bce2736c9eb3a51e8ee8b985a6e788
Brian Carter <bcarter6@me.com>
2025-08-01 16:06:54 -0400

    postgres

---
+++ postgres.R
@@ -0,0 +1,43 @@
+
+
+# TODO:
+
+1.  Implement postgresql to store data
+2.  store metadata in the astroDB
+3.  pull temperature, actual camera temperature, RMS, HFR/FWHM, see what I can add to the graphics.
+
+
+
+
+
+
+library(DBI)
+library(PostgreSQL)
+
+
+username <- Sys.getenv("username")
+password <- Sys.getenv("password")
+
+foo <- dbConnect(dbDriver("PostgreSQL"),
+                 dbname = "astro",
+                 host = "bdcpi",
+                 port = 5432,
+                 user = "briancarter",
+                 password = "Desiree1338",
+                 sslmode = "require")
+con <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  host     = "bdcpi.local",   # or "bdcpi.local"
+  port     = 5432,
+  dbname   = "briancarter",          # don't leave dbname commented out
+  user     = "briancarter",
+  password = "Desiree1338",
+  sslmode  = "require"         # matches a hostssl policy; remove if you allow non-SSL
+)
+
+
+dbListTables(con)
+dbCreateTable(con, "astroDB", data.frame())
+dbWriteTable(con, "astroDB", data.frame(), row.names = FALSE, append = TRUE)
+dbListTables(con)
+dbDisconnect(con)
