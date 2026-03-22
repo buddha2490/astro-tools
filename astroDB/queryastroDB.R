43e8aab6f1330e75804e3e53a03f7ecf88539da8
Brian Carter <bcarter6@me.com>
2026-03-22 10:33:30 -0400

    final

--- astroDB/queryastroDB.R
+++ astroDB/queryastroDB.R
@@ -8,23 +8,48 @@ options(dplyr.summarise.inform = FALSE)
 "/Volumes/Office-SSD/Astronomy/astro-tools/astroDB/functions/functions.R" %>% source()
 
 
+
+# quick access quality data
+meta <- getObjectMetadata("IC2177")
+head(meta[[1]])
+lapply(meta, function(x) {
+  summary(x$FWHM)
+})
+
+
 # Run ------------------------------------------------------------------
 
-myObject <- "NGC281"
+myObject <- "M51"
+
+
 
 # Check the most recent logs
 # Provides a summary of the most recent update time by Status
 checkLogs() 
 
 # Creates a .txt file on desktop for astrobin upload
-astrobinCSV(myObject, csv = FALSE)
+astrobinCSV(myObject, csv = F)
 
 # Query the total integration for an object
-objectTotalIntegration(myObject)
+objectTotalIntegration(myObject="M51")
 
 # Get the other metadata, guiding, HFR, FWHM, etc
-objectMetaData(myObject)
+objectMetaData(myObject = "M33")
 
 
 # list all the objects and total integration time
 dbSummary()
+
+con <- connectDB()
+
+
+df <- tbl(con, "astroSubs") %>%
+  filter(Object == "M51") %>%
+  collect() 
+
+table(df$Object)
+
+
+
+
+dbWriteTable(con, "astroSubs", df, overwrite = TRUE)
